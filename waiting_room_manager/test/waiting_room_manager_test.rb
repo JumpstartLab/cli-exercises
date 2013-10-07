2630b7795fbc5c029b1cafbf205885e82de84036
Franklin Webber <franklin.webber@gmail.com>
2013-10-06 21:50:27 -0600

    Additional test for the Waiting Room

--- waiting_room_manager/test/waiting_room_manager_test.rb
+++ waiting_room_manager/test/waiting_room_manager_test.rb
@@ -6,6 +6,21 @@ require './lib/waiting_room_manager'
 
 class WaitingRoomManagerTest < Minitest::Test
 
+
+  #
+  # Dear Developer
+  #
+  #   I heard about the great job you did for the Kitchen Calculator app so we
+  # are asking you to build our new app to use for our Doctor waiting rooms.
+  # We want to track the people that come into the office, see the doctor, and
+  # leave the office (when they get tired of waiting). We think if we can start
+  # tracking the people we'll stop losing them. It's becoming a problem!
+  #
+  #
+  #                                   Sincerely,
+  #
+  #                                       Patrick Erasmus, M.D.
+  #
   def test_specify_the_current_room_full_of_people
     skip
 
@@ -71,6 +86,32 @@ class WaitingRoomManagerTest < Minitest::Test
     manager.evaluate("leave Jeff")
 
     expected_output = "There are 4 people in the room\nJeff is tired of waiting and leaves the room. There are now 3 people in the room.\n"
+
+    assert_equal expected_output, manager.output
+  end
+
+  #
+  # Dear Developer
+  #
+  #   It's great what you have created so far. I think our lost patient rate has
+  # definitely gotten better. I think if we had time, I think we should start
+  # tracking who has actually seen the doctor.
+  #
+  #
+  #                                   Sincerely,
+  #
+  #                                       Patrick Erasmus, M.D.
+  #
+  def test_has_seen_the_doctor_list
+    skip
+
+    manager = WaitingRoomManager.new
+    manager.evaluate("room Frank, Katrina, Jorge, Jeff")
+    manager.evaluate("call Jeff")
+    manager.evaluate("call Katrina")
+    manager.evaluate("seen doctor")
+
+    expected_output = "There are 4 people in the room\nJeff will now see the doctor. There are now 3 people in the room.\nKatrina will now see the doctor. There are now 2 people in the room.\nJeff and Katrina has seen the doctor.\n"
 
     assert_equal expected_output, manager.output
   end
